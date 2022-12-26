1f6e7f878fff2ec6a27a496b94713310f09a2357
Xebec19 <rohant946@gmail.com>
2022-12-26 20:42:06 +0530

    chore: set dotenv vault

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,8 @@
-import * as de from "dotenv";
+import * as de from 'dotenv';
+
 const dotenv = de.config();
 if (dotenv.error) {
-  console.error("Error occurred while setting dot env files : ", dotenv.error);
+  console.error('Error occurred while setting dot env files : ', dotenv.error);
 }
 export const dbUser = process.env.DB_USER;
 export const dbHost = process.env.DB_HOST;
